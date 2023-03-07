73c9725c29fc0d79d28df50559179452d416e576
Avramu98 <vlad_avram98@yahoo.com>
2023-03-07 18:05:51 +0200

    configured tailwind

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  experimental: {
+    optimizeCss: true,
+    fontLoaders: [
+      {loader: '@next/font/google', options: {subsets: ['latin']}}
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Press_Start_2P, Encode_Sans_Condensed } from '@next/font/google';
+import Head from 'next/head';
+
+const enhanced = Press_Start_2P({
+  weight: ['400'],
+  display: 'swap',
+  subsets: ['latin'],
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+const standard = Encode_Sans_Condensed({
+  weight: ['400'],
+  display: 'swap',
+  subsets: ['latin'],
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width"/>
+        </Head>
+        <style jsx global>
+          {`
+            :root {
+              --enhanced-font: ${enhanced.style.fontFamily};
+              --standard-font: ${standard.style.fontFamily};
+            } 
+          `}
+        </style>
+       <Component {...pageProps} />
+      </>
+  );
 }
+  
